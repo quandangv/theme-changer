c8555477e127c9fbbdace07d4850cd907a2bb65d
quandangv <quandangv@gmail.com>
2021-04-16 23:24:08 +0700

    Make it easier to add/remove apps support in themes

--- theme-changer.sh
+++ theme-changer.sh
@@ -1,14 +1,26 @@
-termite_path=~/.config/termite
-kakoune_path=~/.config/kak
-bspwm_path=~/.config/kak
-yuzubar_path=~/.config/yuzubar
+config_termite="templates/termite  ~/.config/termite/config"
+config_kakoune="templates/kakoune  ~/.config/kak/colors/default.kak"
+config_bspwm="templates/bspwm      ~/.config/kak/bspwmrc"
+config_yuzubar="templates/yuzubar  ~/.config/yuzubar/default.yzb"
+config_fehbg="templates/fehbg      ~/.fehbg"
 
-linked_replace -i "$1" templates/termite $termite_path/config  templates/kakoune $kakoune_path/colors/default.kak templates/bspwm $bspwm_path/bspwmrc templates/fehbg ~/.fehbg templates/yuzubar $yuzubar_path/default.yzb
+cmd="linked_replace -i \"$1\" $config_termite $config_kakoune $config_bspwm $config_fehbg $config_yuzubar"
+echo $cmd
+eval $cmd
 
+# Reload termite
 pkill --signal USR1 termite
+
+# Reload yuzubar
 pkill --signal USR1 yuzubar
+
+# Reload bspwm
 bspc wm -r
+
+# Reload fehbg
 ~/.fehbg
+
+# Reload kakoune
 while IFS= read -r line; do
   if [[ "$line" != *"(dead)"* ]]; then
     echo colorscheme default | kak -p $line
